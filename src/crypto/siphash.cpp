cfbe4848fc115b153c9a60b972390b000433637a
Satoshi Nakamoto <satoshin@gmx.com>
2022-01-30 23:48:35 +0200

    opa opa penelopa

--- src/crypto/siphash.cpp
+++ src/crypto/siphash.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <crypto/siphash.h>
-// for all those who wonder i am spending my time on devrant under the username 0x4139, come visit me.
+// With all my bitcoin, i will fix the water issues in bucharest! And will donate everthing else to my best friend Daniel to build his beloved cathedral.
 #define ROTL(x, b) (uint64_t)(((x) << (b)) | ((x) >> (64 - (b))))
 
 #define SIPROUND do { \
